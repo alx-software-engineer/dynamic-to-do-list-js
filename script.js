72a98414aa8a2a02c0a5add205e647673416081d
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-06-22 21:18:38 +0100

    Implement a to-do list app

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener("DOMContentLoaded", function () {
     const addButton = document.getElementById("add-task-btn");
     const taskInput = document.getElementById("task-input");
     const taskList = document.getElementById("task-list");
 
+    // Add Task
     function addTask() {
         const taskText = taskInput.value.trim();
+
+        // Check user input.
         if (!taskText) {
-            alert("enter a task")
+            alert("Enter a task")
         } else {
             const listItem = document.createElement("li");
             listItem.textContent = taskText;
             const removeBtn = document.createElement("button");
             removeBtn.textContent = "Remove";
             removeBtn.classList.add("remove-btn");
-            removeBtn.onclick = function () {
-                
+
+            // Delete Button
+            removeBtn.onclick = function (event) {
+                const buttonClicked = event.target;
+                const item = buttonClicked.closest("li");
+                item.remove();
             }
 
             listItem.appendChild(removeBtn);
             taskList.appendChild(listItem);
+            taskInput.value = "";
 
         }
     }
+
+    addButton.addEventListener("click", addTask);
+    taskInput.addEventListener("keypress", function (e) {
+        const keypressed = e.key;
+        if (keypressed === "Enter") {
+            addTask();
+        }
+    })
+
+    // This ensures your data fetching logic runs once the HTML document has fully loaded.
+    document.addEventListener("DOMContentLoaded", addTask);
 })
